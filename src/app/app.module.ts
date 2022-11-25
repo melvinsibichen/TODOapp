699bb497038813c75093cc35058f6c8ff51ffdee
melvinsibichen <melvinsibichen1020@gmail.com>
2022-11-25 12:56:07 +0530

    navbar created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddtodoComponent } from './addtodo/addtodo.component';
 import { ViewtodoComponent } from './viewtodo/viewtodo.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddtodoComponent,
-    ViewtodoComponent
+    ViewtodoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
